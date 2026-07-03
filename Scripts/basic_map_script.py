a75d2c680d464df616c6b9ec6753773cc7317aa0
mackinamm-web <mackinamm@gmail.com>
2026-07-03 20:13:29 +0100

    Clean and update base map code in notebook
    Cleaned and reorganised the basic map script. Added the mapping code to the notebook in structured sections

--- Scripts/basic_map_script.py
+++ Scripts/basic_map_script.py
@@ -1,3 +1,4 @@
+# Imports for geospatial data, mapping, and plotting
 import os
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -6,7 +7,7 @@ import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 
-
+# Helper functions for legend handles and scale bar
 def generate_handles(labels, colors, edge='k', alpha=1):
     """
     Generate matplotlib patch handles to create a legend of each of the features in the map.
@@ -38,8 +39,6 @@ def generate_handles(labels, colors, edge='k', alpha=1):
         handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[ii % lc], edgecolor=edge, alpha=alpha))
     return handles
 
-# adapted this question: https://stackoverflow.com/q/32333870
-# answered by SO user Siyh: https://stackoverflow.com/a/35705477
 def scale_bar(ax, length=20, location=(0.92, 0.95)):
     """
     Create a scale bar in a cartopy GeoAxes.
@@ -75,48 +74,53 @@ def scale_bar(ax, length=20, location=(0.92, 0.95)):
 
     return ax
 
-# load the datasets
 
+# Load outline and water shapefiles
 outline = gpd.read_file('../Data/NI_outline.shp')
 water = gpd.read_file('../Data/Water.shp')
 
-ni_utm = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
-# NI is in UTM Zone 29, so we pass 29 to ccrs.UTM()
 
-fig = plt.figure(figsize=(8, 8))  # create a figure of size 8x8 (representing the page size in inches)
-ax = plt.axes(projection=ni_utm)  # create an axes object in the figure, using a UTM projection,
-# where we can actually plot our data.
+# Set Up Map Projection and Figure
 
-# first, we just add the outline of Northern Ireland using cartopy's ShapelyFeature
+# UTM Zone 29 (NI)
+ni_utm = ccrs.UTM(29)  
+
+# Create a figure size 8x8
+fig = plt.figure(figsize=(8, 8))
+
+# Creates axes with UTM projection
+ax = plt.axes(projection=ni_utm)  
+
+# Add NI outline
 outline_feature = ShapelyFeature(outline['geometry'], ni_utm, edgecolor='k', facecolor='w')
+
+# Add all features to the map
 ax.add_feature(outline_feature) # add the features we've created to the map.
 
+# Map extent using outline bounds
 xmin, ymin, xmax, ymax = outline.total_bounds
-# using the boundary of the shapefile features, zoom the map to our area of interest
-ax.set_extent([xmin-5000, xmax+5000, ymin-5000, ymax+5000], crs=ni_utm)  # because total_bounds
-# gives output as xmin, ymin, xmax, ymax,
-# but set_extent takes xmin, xmax, ymin, ymax, we re-order the coordinates here.
+ax.set_extent([xmin-5000, xmax+5000, ymin-5000, ymax+5000], crs=ni_utm)  
 
-# here, we're setting the edge color to be the same as the face color. Feel free to change this around,
-# and experiment with different line widths.
-water_feat = ShapelyFeature(water['geometry'], # first argument is the geometry
-                            ccrs.CRS(water.crs), # second argument is the CRS
-                            edgecolor='mediumblue', # set the edgecolor to be mediumblue
-                            facecolor='mediumblue', # set the facecolor to be mediumblue
-                            linewidth=1) # set the outline width to be 1 pt
-ax.add_feature(water_feat) # add the collection of features to the map
+# Add the water features and set CRS, colours and outline width
+water_feat = ShapelyFeature(water['geometry'], 
+                            ccrs.CRS(water.crs), 
+                            edgecolor='mediumblue',
+                            facecolor='mediumblue', 
+                            linewidth=1) 
+ax.add_feature(water_feat) 
 
-# note: if you change the color you use to display lakes, you'll want to change it here, too
+# Create legend
 water_handle = generate_handles(['Lakes'], ['mediumblue'])
 
+# Add gridlines
 gridlines = ax.gridlines(draw_labels=True, # draw  labels for the grid lines
                          xlocs=[-8, -7.5, -7, -6.5, -6, -5.5], # add longitude lines at 0.5 deg intervals
                          ylocs=[54, 54.5, 55, 55.5]) # add latitude lines at 0.5 deg intervals
 gridlines.left_labels = False # turn off the left-side labels
 gridlines.bottom_labels = False # turn off the bottom labels
 
-# add the scale bar to the axis
+# Add a scale bar to the axis
 scale_bar(ax)
 
-# save the figure as map.png, cropped to the axis (bbox_inches='tight'), and a dpi of 300
+# save fig as lough_neagh_map.png, crop to the axis ('tight') with dpi of 300
 fig.savefig('map.png', bbox_inches='tight', dpi=300)
